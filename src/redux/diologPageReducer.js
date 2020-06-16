bf2eed336d599707db3165fdef0d6f2230b47977
KrasavinSerg <krasavin.serg@gmail.com>
2020-06-16 23:31:31 +0300

    reducer copys

--- src/redux/diologPageReducer.js
+++ src/redux/diologPageReducer.js
@@ -17,17 +17,24 @@ let initialState = {
 
 const diologPageReducer = (state = initialState, action) => {
   switch (action.type) {
-    case ADD_MESSAGE:
+    case ADD_MESSAGE: {
       let newMessage = {
         message : state.newMessageText
       };
-    
-      state.messageData.push(newMessage);
-      state.newMessageText = '';
-      return state;
-    case UPDATE_NEW_MESSAGE_TEXT:
-      state.newMessageText = action.newText;
-      return state;
+
+      let stateCopy = {...state};
+      stateCopy.messageData = [...state.messageData];
+      stateCopy.messageData.push(newMessage);
+      stateCopy.newMessageText = '';
+      return stateCopy;
+    }
+
+    case UPDATE_NEW_MESSAGE_TEXT: {
+      let stateCopy = {...state};
+      stateCopy.newMessageText = action.newText;
+      return stateCopy;
+    }
+
     default:
       return state;  
   }
