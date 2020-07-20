334a4331524ffe697d9727b197d3a42ecd5e4e50
KrasavinSerg <krasavin.serg@gmail.com>
2020-07-20 21:48:14 +0300

    DAL, axios.create

---
+++ src/api/api.js
@@ -0,0 +1,18 @@
+import * as axios from 'axios';
+
+const instance = axios.create({
+  withCredentials: true,
+  baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+  headers: {
+    'API-KEY': 'cfbce4cf-eeea-4650-8beb-2254170799ce'
+  }
+});
+
+export const usersAPI = {
+  getUsers(currentPage = 1 , pageSize = 10) {
+    return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+    .then(response => {
+      return response.data;
+    })
+  } 
+} 
